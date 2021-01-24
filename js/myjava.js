1ff78aaf31a532730629056d78cb203a204bd164
koomsone <43884002+koomsone@users.noreply.github.com>
2021-01-24 20:28:20 +0000

    Update myjava.js

--- js/myjava.js
+++ js/myjava.js
@@ -2,5 +2,5 @@ var string = document.lastModified;
 			document.write(document.lastModified);
 			// returns: saturday, December 16, 2017 11:09:42
 			
-var d = new Date("2021");
+var d = new Date();
 document.getElementById("new_Date").innerHTML = d.getFullYear();
