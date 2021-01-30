e40497092f202e3ed0001a091e145b75d8f45a2b
koomsone <43884002+koomsone@users.noreply.github.com>
2021-01-30 22:31:07 +0000

    Add files via upload

---
+++ lesson 2/js/designprinciples.js
@@ -0,0 +1,6 @@
+var string = document.lastModified;
+			document.write(document.lastModified);
+			// returns: saturday, December 16, 2017 11:09:42
+			
+var d = new Date();
+document.getElementById("new_Date").innerHTML = d.getFullYear();
