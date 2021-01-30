7e66fea844c7ad83fa7026903e30862d92067d3c
koomsone <43884002+koomsone@users.noreply.github.com>
2021-01-30 23:25:10 +0000

    Delete designprinciples.js

--- lesson 2/js/designprinciples.js
+++
@@ -1,6 +0,0 @@
-var string = document.lastModified;
-			document.write(document.lastModified);
-			// returns: saturday, December 16, 2017 11:09:42
-			
-var d = new Date();
-document.getElementById("new_Date").innerHTML = d.getFullYear();
